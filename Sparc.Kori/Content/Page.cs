12ad77220ef264b5b9e27804d7976cfa16552da8
Antonio Correia <antonio.alves.correia@gmail.com>
2024-11-05 20:46:43 -0300

    Starting KoriContext

---
+++ Sparc.Kori/Content/Page.cs
@@ -0,0 +1,20 @@
+﻿using Sparc.Blossom.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparc.Kori.Content
+{
+    public class Page : BlossomEntity<string>
+    {
+        public string PageId { get; private set; }
+        public string PageType { get; private set; }
+        public string Name { get; private set; }
+        public string Slug { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime? LastActiveDate { get; private set; }
+        public DateTime? EndDate { get; private set; }
+    }
+}
---
+++ Sparc.Kori/_Plugins/KoriContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sparc.Kori.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparc.Kori._Plugins
+{
+    public class KoriContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Page> Pages => Set<Page>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Page>().ToContainer("Pages").HasPartitionKey(x => x.PageId);
+        }
+    }
+}
