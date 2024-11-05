a1d1a4639fc8c7345e6c720eb2be554afb527492
Christine Antonio <christinejm.antonio@gmail.com>
2024-11-05 13:06:23 -0800

    copy KoriLogin files over and fix references

--- Sparc.Kori/wwwroot/js/Kori.js
+++ Sparc.Kori/wwwroot/js/Kori.js
@@ -269,11 +269,48 @@ function mouseClickHandler(e) {
         }
     }
 
+    if (t.closest(".kori-login__tab")) {
+        if (tabsParent && tabs) {
+            if (tabs.length > 0) {
+                tabs.forEach((tab) => {
+                    // Remove active class
+                    tabs.forEach((t, i) => {
+                        t.classList.remove("active");
+                    });
+                })
 
-        // click kori enabled elements        
-        toggleSelected(t);
-        toggleTopBar(t);
+                // Add active class to clicked tab
+                t.classList.add("active");
+                updateActiveIndicator(t);
+            }
+        }
+    }
 
+    // click kori enabled elements        
+    toggleSelected(t);
+    toggleTopBar(t);
+}
+
+// global login - mobile UI, tabs sliding active indicator
+
+var tabsParent = document.getElementById("kori-login__tabs");
+var tabs = document.querySelectorAll(".kori-login__tab");
+
+function updateActiveIndicator(activeElement) {
+    const tabsParentLeftDistance = tabsParent.getBoundingClientRect().left;
+    console.log("tabsParentLeftDistance: " + tabsParentLeftDistance);
+
+    const {
+        width: elementSize,
+        left: elementLeftDistance,
+    } = activeElement.getBoundingClientRect();
+
+    const distanceFromParent = elementLeftDistance - tabsParentLeftDistance;
+    console.log("distancefromParent: " + distanceFromParent);
+    console.log("elementSize: " + elementSize);
+
+    tabsParent.style.setProperty("--indicator-offset", distanceFromParent + "px");
+    tabsParent.style.setProperty("--indicator-width", elementSize + "px");
 }
 
 // selecting and unselecting kori-enabled elements
