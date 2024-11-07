28c24b8e3857c4c3508ee76d70ee85b87c4287e2
Joseph Young <joseph@josephayoung.com>
2024-11-07 10:34:04 -0600

    Add KoriApplication static class for quick initialization

---
+++ Sparc.Kori/KoriApplication.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Sparc.Blossom;
+using Sparc.Blossom.Authentication;
+using Sparc.Blossom.Realtime;
+using System.Reflection;
+
+namespace Sparc.Kori;
+
+public static class KoriApplication
+{
+    public static WebApplication Run<TApp>(
+        string[] args,
+        Uri baseUri,
+        Action<WebApplicationBuilder>? builderOptions = null,
+        Action<WebApplication>? appOptions = null,
+        IComponentRenderMode? renderMode = null,
+        Assembly? apiAssembly = null)
+    {
+        return BlossomApplication.Run<TApp>(args, builder =>
+        {
+            builderOptions?.Invoke(builder);
+            builder.AddKori(baseUri);
+        }, app =>
+        {
+            appOptions?.Invoke(app);
+            app.UseKori();
+        }, renderMode, apiAssembly);
+    }
+
+    public static WebApplication Run<TApp, TUser>(
+        string[] args,
+        Uri baseUri,
+        Action<WebApplicationBuilder>? builderOptions = null,
+        Action<WebApplication>? appOptions = null,
+        IComponentRenderMode? renderMode = null)
+        where TUser : BlossomUser, new()
+    {
+        return BlossomApplication.Run<TApp, TUser>(args, builder =>
+        {
+            builderOptions?.Invoke(builder);
+            builder.AddKori(baseUri);
+        }, app =>
+        {
+            appOptions?.Invoke(app);
+            app.UseKori();
+        }, renderMode);
+    }
+
+    public static WebApplication Run<TApp, TUser, THub>(
+        string[] args,
+        Uri baseUri,
+        Action<WebApplicationBuilder>? builderOptions = null,
+        Action<WebApplication>? appOptions = null,
+        IComponentRenderMode? renderMode = null)
+        where TUser : BlossomUser, new()
+        where THub : BlossomHub
+    {
+        return BlossomApplication.Run<TApp, TUser, THub>(args, builder =>
+        {
+            builderOptions?.Invoke(builder);
+            builder.AddKori(baseUri);
+        }, app =>
+        {
+            appOptions?.Invoke(app);
+            app.UseKori();
+        }, renderMode);
+    }
+}
