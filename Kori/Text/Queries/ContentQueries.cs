c858716aa14f8b4aec44108848d136c311e4f7b6
Laura Ferraz <lauraferraz5@gmail.com>
2024-11-18 17:45:27 -0300

    Add SearchContent query

---
+++ Kori/Text/Queries/ContentQueries.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using Kori;
+using Sparc.Blossom.Data;
+
+public class Search : BlossomQuery<Content>
+{
+    public Search(string searchTerm) => Query.Where(content =>           
+        ((content.Text != null && content.Text.ToLower().Contains(searchTerm) == true) ||
+         (content.Tag != null && content.Tag.ToLower().Contains(searchTerm) == true) ||
+         (content.Domain != null && content.Domain.ToLower().Contains(searchTerm) == true) ||
+         (content.Path != null && content.Path.ToLower().Contains(searchTerm) == true)));
+}
