127da2a6560eb05fafe41a69e8dea92f712ba45c
Laura Ferraz <lauraferraz5@gmail.com>
2024-11-21 16:28:42 -0300

    Search mode working

--- Sparc.Kori/Search/KoriSearchEngine.cs
+++ Sparc.Kori/Search/KoriSearchEngine.cs
@@ -1,8 +1,6 @@
-﻿using static System.Net.WebRequestMethods;
+﻿namespace Sparc.Kori;
 
-namespace Sparc.Kori;
-
-//public record KoriSearch(string ContentId, string Tag, string Text, string Domain, string Path);
+public record KoriSearch(string Id, string Tag, string Text, string Domain, string Path, string Source);
 
 public class KoriSearchEngine(KoriHttpEngine http, KoriJsEngine js)
 {
@@ -10,16 +8,30 @@ public class KoriSearchEngine(KoriHttpEngine http, KoriJsEngine js)
     {
         await js.InvokeVoidAsync("showSidebar");
     }
-    
+
     public async Task CloseAsync()
     {
         Console.WriteLine("Closing search side bar in Kori.cs");
         await js.InvokeVoidAsync("closeSearch");
-    }
+    }  
 
     public async Task<List<KoriSearch>> SearchAsync(string searchTerm)
     {
-        await http.SearchContentAsync(searchTerm);
-        await http.SearchPageAsync(searchTerm);
+        var contentResults = await http.SearchContentAsync(searchTerm);
+        var pageResults = await http.SearchPageAsync(searchTerm);
+
+        var mappedContentResults = contentResults
+           .Select(content =>
+               new KoriSearch(content.Id, content.Tag, content.Text, content.Domain, content.Path, "Content"))
+           .ToList();
+
+        var mappedPageResults = pageResults
+            .Select(page =>
+                new KoriSearch(page.Id, null, null, page.Domain, page.Path ?? "", "Page"))
+            .ToList();
+
+        var combinedResults = mappedContentResults.Concat(mappedPageResults).ToList();
+
+        return combinedResults;
     }
 }
