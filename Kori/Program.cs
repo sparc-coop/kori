3d569dab3ae34ee1618433c54708190d8fbc39e5
Joseph Young <joseph@josephayoung.com>
2025-01-02 08:29:55 -0600

    Add Configuration and IsDevelopment to Blossom App builder, move Scalar to Blossom

--- Kori/Program.cs
+++ Kori/Program.cs
@@ -1,19 +1,15 @@
 ﻿using Kori;
 using Scalar.AspNetCore;
 
-BlossomApplication.Run<Html>(args, builder =>
-{
-    builder.Services.AddCosmos<KoriContext>(builder.Configuration.GetConnectionString("CosmosDb")!, "kori", ServiceLifetime.Scoped)
+var builder = BlossomApplication.CreateBuilder(args);
+
+builder.Services.AddCosmos<KoriContext>(builder.Configuration.GetConnectionString("CosmosDb")!, "kori", ServiceLifetime.Scoped)
         .AddAzureStorage(builder.Configuration.GetConnectionString("Storage")!)
         .AddScoped<Translator>()
         .AddScoped<ITranslator, DeepLTranslator>()
         .AddScoped<ITranslator, AzureTranslator>()
         .AddScoped<ISpeaker, AzureSpeaker>();
 
-    // builder.AddKori(new Uri("https://kori.app"));
-},
-app =>
-{
-    if (app.Environment.IsDevelopment())
-        app.MapScalarApiReference();
-});
+var app = builder.Build();
+
+await app.RunAsync<Html>();
