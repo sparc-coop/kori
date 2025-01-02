96ab7e8f2d8dc01201fc89e9e1b39d70459b955f
Joseph Young <joseph@josephayoung.com>
2025-01-02 10:59:31 -0600

    Simplify and setup API endpoints in Sparc.Kori

--- Sparc.Kori/ServiceCollectionExtensions.cs
+++ Sparc.Kori/ServiceCollectionExtensions.cs
@@ -1,22 +1,23 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http.Json;
-using System.Text.Json;
 
 namespace Sparc.Kori;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddKori(this IServiceCollection services, Uri baseUri)
+    public static IServiceCollection AddKori(this IServiceCollection services, Uri baseUri, Uri? koriApiUri = null)
     {
         services.AddLocalization();
-        // services.AddHttpClient<KoriHttpEngine>(client => client.BaseAddress = new Uri("https://localhost:53579"));
+        koriApiUri ??= new Uri("https://kori.page");
+        services.AddBlossomApi<KoriTextContent>(koriApiUri, "Contents");
+        services.AddBlossomApi<KoriLanguage>(koriApiUri, "Languages");
+        services.AddBlossomApi<KoriPage>(koriApiUri, "Pages");
+
 
         services.AddScoped<KoriEngine>()
             .AddScoped<KoriLanguageEngine>()
             .AddScoped<KoriContentEngine>()
             .AddScoped<KoriSearchEngine>()
             .AddScoped<KoriImageEngine>()
-            .AddScoped<KoriHttpEngine>()
             .AddScoped<KoriJsEngine>();
 
         KoriEngine.BaseUri = baseUri;
