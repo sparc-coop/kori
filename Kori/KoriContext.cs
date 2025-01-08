2c2a94eaee39f81a684a905d1e8634538969a9a0
Joseph Young <joseph@josephayoung.com>
2025-01-08 09:48:05 -0600

    Add KoriUser to Kori context

--- Kori/KoriContext.cs
+++ Kori/KoriContext.cs
@@ -6,8 +6,8 @@ public class KoriContext(DbContextOptions options) : DbContext(options)
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<Page>().HasKey(x => new { x.Domain, x.Path });
         builder.Entity<Page>().HasPartitionKey(x => new { x.Domain, x.Path }).ToContainer("content");
         builder.Entity<Content>().HasPartitionKey(x => new { x.Domain, x.Path }).ToContainer("content");
+        builder.Entity<KoriUser>().HasPartitionKey(x => x.UserId).ToContainer("users");
     }
 }
