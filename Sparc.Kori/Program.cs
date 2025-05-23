4bf0fb2a66a27289714800f5f3c97242ee170162
Antonio Correia <antonio.alves.correia@gmail.com>
2025-05-23 00:27:06 -0300

    adding blossom cloud api to kori project

--- Sparc.Kori/Program.cs
+++ Sparc.Kori/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Refit;
+using Sparc.Blossom.Cloud.Api;
 using Sparc.Kori;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddRefitClient<IBlossomCloudApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7185"));
 
 await builder.Build().RunAsync();
