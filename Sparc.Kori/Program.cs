8faed9078e9078b6199424aab8e73caeecb404d6
Antonio Correia <antonio.alves.correia@gmail.com>
2025-05-25 23:33:29 -0300

    Adding sample project with Blossom auth and listing user products

--- Sparc.Kori/Program.cs
+++ Sparc.Kori/Program.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Sparc.Kori;
+﻿using Sparc.Kori.Components;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = BlossomApplication.CreateBuilder<App>(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.AddBlossomCloud();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+await app.RunAsync<App>();
+
