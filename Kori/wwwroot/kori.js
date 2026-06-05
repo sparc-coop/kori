440ea13fd4c01b9815f7ce94a9e65be49519aaf4
Joseph Young <joseph@josephayoung.com>
2026-06-05 10:15:22 -0500

    Implemented realtime local-first edit algorithm for Kori

--- Kori/wwwroot/kori.js
+++ Kori/wwwroot/kori.js
@@ -5994,10 +5994,9 @@
     setDebug(debug);
 
     const db = new Dexie$1('TovikTranslate');
-    db.version(4).stores({
+    db.version(5).stores({
         translations: 'id',
-        languages: 'id',
-        profiles: 'id'
+        edits: 'id'
     });
 
     class TovikEngine {
@@ -6262,6 +6261,8 @@
         iframe;
         mode;
         debounceSave;
+        userId;
+        history;
         constructor() {
             super();
             // Lock the context of these functions to this class, so they can be added and removed as event listeners without losing the context of 'this'
@@ -6273,6 +6274,7 @@
             this.endEdit = this.endEdit.bind(this);
             this.save = this.save.bind(this);
             this.debounceSave = debounce(this.save, 500);
+            this.localSave = this.localSave.bind(this);
         }
         async connectedCallback() {
             // Add 2 boxes to this custom element, to be positioned absolutely on top of the target element as bordered identifiers for the element
@@ -6290,7 +6292,8 @@
             this.iframe.classList.add('kori-iframe');
             this.iframe.src = `${TovikEngine.widgetUrl}/sites/${domain}/widget?_wauth=${authCode}`;
             this.appendChild(this.iframe);
-            BlossomEvents.on('mode', (mode) => this.setMode(mode));
+            this.setMode('Edit');
+            BlossomEvents.on('initialized', (userId) => this.userId = userId);
             BlossomEvents.on('bold', () => this.format('bold'));
             BlossomEvents.on('italic', () => this.format('italic'));
             BlossomEvents.on('saved', (item) => TovikEngine.update(item));
@@ -6313,12 +6316,6 @@
                 document.addEventListener('mouseover', this.highlightTarget);
                 document.addEventListener('click', this.beginEdit);
                 document.addEventListener('scroll', this.positionBoxes);
-            }
-            else if (this.mode == 'Edit') {
-                document.removeEventListener('mouseover', this.highlightTarget);
-                document.removeEventListener('click', this.beginEdit);
-                document.removeEventListener('scroll', this.positionBoxes);
-                this.markTarget(null);
             }
             this.mode = mode;
         }
@@ -6358,7 +6355,7 @@
             this.horizontalBox.style.height = `${rect.height}px`;
             this.horizontalBox.style.display = 'block';
         }
-        beginEdit(event) {
+        async beginEdit(event) {
             if (!this.isEditable(event.target))
                 return;
             event.preventDefault();
@@ -6368,9 +6365,50 @@
                 this.target = event.target;
                 this.target.contentEditable = true;
                 this.target.focus();
-                this.target.addEventListener('input', this.debounceSave);
+                if (!this.target.hash)
+                    this.target.hash = TovikEngine.idHash(this.textNode(this.target)['originalText']);
+                this.history = await db.edits.get(this.target.hash);
+                if (!this.history) {
+                    this.history = { id: this.target.hash, clock: 0, edits: [], current: [] };
+                    this.initializeCurrent(this.target);
+                }
+                this.target.addEventListener('input', this.localSave);
                 event.stopPropagation();
             }
+        }
+        initializeCurrent(target) {
+            const originalText = this.textNode(target)['originalText'];
+            this.history.current = originalText.split('').map((char) => ({ opId: `${++this.history.clock}@original`, value: char, deleted: false }));
+            this.history.clock++;
+        }
+        async localSave(inputEvent) {
+            this.history.clock++;
+            let cursor = this.cursorPosition();
+            const action = inputEvent.inputType.startsWith('delete') ? 'delete' : inputEvent.inputType.startsWith('insert') ? 'insert' : inputEvent.inputType;
+            const current = this.history.current.filter(x => !x.deleted);
+            const refId = cursor && current.length > cursor ? current[cursor].opId : null;
+            const edit = {
+                opId: `${this.history.clock}@${this.userId}`,
+                refId: refId,
+                action: action,
+                value: inputEvent.data,
+            };
+            this.history.edits.push(edit);
+            // Process the operation
+            if (action === 'insert') {
+                const character = { opId: edit.opId, value: edit.value, deleted: false };
+                if (!refId)
+                    this.history.current.push(character);
+                else
+                    this.history.current.splice(current.findIndex(x => x.opId === refId) + 1, 0, character);
+            }
+            else if (action === 'delete') {
+                const character = this.history.current.find(x => x.opId == refId);
+                if (character)
+                    character.deleted = true;
+            }
+            console.log('edit', cursor, inputEvent.inputType, edit, this.history.current);
+            //await db.edits.put(this.history);
         }
         async save() {
             if (!this.target)
@@ -6391,10 +6429,21 @@
                 this.target.classList.remove('kori-editable');
                 this.target.contentEditable = false;
                 this.target.isDirty = false;
-                this.target.removeEventListener('input', this.debounceSave);
+                this.target.removeEventListener('input', this.localSave);
                 this.target.removeEventListener('blur', this.endEdit);
             }
             this.target = null;
+        }
+        cursorPosition() {
+            let caretPos = 0;
+            const sel = window.getSelection();
+            if (sel.rangeCount) {
+                const range = sel.getRangeAt(0);
+                if (range.commonAncestorContainer.parentNode == this.target) {
+                    caretPos = range.endOffset;
+                }
+            }
+            return caretPos;
         }
         cancel() {
             if (!this.target)
